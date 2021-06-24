f6258a7eefa22139d7686b89f1cfe240a8753b8d
Allan Gallo <allan.gallo.guerra@gmail.com>
2021-06-24 08:07:36 -0600

    ADD - id to gasto object, and route persona

--- server.js
+++ server.js
@@ -1,78 +1,138 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-	const server = Hapi.server({
-		port: process.env.PORT || 3000,
-	});
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+  });
 
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: (request, h) => {
-			return 'Hello from hapijs API'
-		}
-	});
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello from hapijs API';
+    },
+  });
 
-	server.route({
-		method: 'GET',
-		path: '/gasto',
-		handler: (request, h) => {
-			const gastos = [
-				{
-					nombre: 'Viaticos'
-				},
-				{
-					nombre: 'Eventos'
-				},
-				{
-					nombre: 'Gastos menores de operacion'
-				},
-				{
-					nombre: 'Honorarios'
-				},
-				{
-					nombre: 'Comunicaciones'
-				},
-				{
-					nombre: 'Arrendamientos'
-				},
-				{
-					nombre: 'Mensajeria'
-				},
-				{
-					nombre: 'Reparacion y Mantenimientos'
-				},
-			]	
+  server.route({
+    method: 'GET',
+    path: '/gasto',
+    handler: (request, h) => {
+      const gastos = [
+        {
+          id: 1,
+          nombre: 'Viaticos',
+        },
+        {
+          id: 2,
+          nombre: 'Eventos',
+        },
+        {
+          id: 3,
+          nombre: 'Gastos menores de operacion',
+        },
+        {
+          id: 4,
+          nombre: 'Honorarios',
+        },
+        {
+          id: 5,
+          nombre: 'Comunicaciones',
+        },
+        {
+          id: 6,
+          nombre: 'Arrendamientos',
+        },
+        {
+          id: 7,
+          nombre: 'Mensajeria',
+        },
+        {
+          id: 8,
+          nombre: 'Reparacion y Mantenimientos',
+        },
+      ];
 
-			return {
-				success: true,
-				data: gastos,
-				error: ''
-			};
-		}
-		
-	});
+      return {
+        success: true,
+        data: gastos,
+        error: '',
+      };
+    },
+  });
 
-	server.route({
-		method: 'POST',
-		path: '/gasto',
-		handler: (request, h) => {
-			return {
-				success: true,
-				data: [],
-				error: ''
-			}
-		}
-	});
+  server.route({
+    method: 'POST',
+    path: '/gasto',
+    handler: (request, h) => {
+      return {
+        success: true,
+        data: [
+          {
+            ...h.payload,
+          },
+        ],
+        error: '',
+      };
+    },
+  });
 
-	await server.start();
-	console.log('Server running on %s', server.info.uri);
+  server.route({
+    method: 'GET',
+    path: '/persona',
+    handler: (request, h) => {
+      const persona = [
+        {
+          id: 1,
+          nombre: 'Juan Pablo',
+          correo: 'juan@gmail.com',
+        },
+        {
+          id: 2,
+          nombre: 'Pablo',
+          correo: 'pablo@gmail.com',
+        },
+        {
+          id: 3,
+          nombre: 'Julio',
+          correo: 'julio@gmail.com',
+        },
+        {
+          id: 4,
+          nombre: 'Charli',
+          correo: 'charli@gmail.com',
+        },
+        {
+          id: 5,
+          nombre: 'Carlos',
+          correo: 'carlos@gmail.com',
+        },
+        {
+          id: 6,
+          nombre: 'Mario',
+          correo: 'mario@gmail.com',
+        },
+        {
+          id: 7,
+          nombre: 'Maria',
+          correo: 'maria@gmail.com',
+        },
+      ];
 
+      return {
+        success: true,
+        data: persona,
+        error: '',
+      };
+    },
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-	console.log(err);
-	process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
