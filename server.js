7c76931bf5aa0dabd657a5a9673f4eaa7eb05fb3
Allan Gallo <allan.gallo.guerra@gmail.com>
2021-06-23 23:17:03 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: 3000,
+		host: 'localhost'
+	});
+
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: (request, h) => {
+			const gastos = [
+				{
+					nombre: 'Viaticos'
+				},
+				{
+					nombre: 'Eventos'
+				},
+				{
+					nombre: 'Gastos menores de operacion'
+				},
+				{
+					nombre: 'Honorarios'
+				},
+				{
+					nombre: 'Comunicaciones'
+				},
+				{
+					nombre: 'Arrendamientos'
+				},
+				{
+					nombre: 'Mensajeria'
+				},
+				{
+					nombre: 'Reparacion y Mantenimientos'
+				},
+			]	
+
+			return {
+				success: true,
+				data: gastos,
+				error: ''
+			};
+		}
+		
+	});
+
+	server.route({
+		method: 'POST',
+		path: '/',
+		handler: (request, h) => {
+			return {
+				success: true,
+				data: [],
+				error: ''
+			}
+		}
+	});
+
+	await server.start();
+	console.log('Server running on %s', server.info.uri);
+
+};
+
+process.on('unhandledRejection', (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
